0986b578bfc14d762fd0a51f0fab556704734033
xhdong <xhdonguser@sina.com>
2023-05-16 09:05:59 +0800

    upd: optmize chat detail

--- miniprogram/pages/chat/chat.ts
+++ miniprogram/pages/chat/chat.ts
@@ -3,11 +3,12 @@ const { completions } = require('../../api/chat')
 
 Page({
   data: {
-    loading: !0,
+    loading: false,
     inputValue: '',  // 输入框的值
     // 聊天记录
     msgList: [
       {
+        loading: false,
         role: 'assistant',
         content: '你好呀，想问什么就问吧'
       }
@@ -33,18 +34,14 @@ Page({
 
   // 发送用户消息
   sendUserMsg(question: string) {
-    this.setData({
-      loading: true
-    })
     const userMsg = {
       role: 'user',
+      loading: false,
       content: question
     }
     this.data.msgList.push(userMsg)
     this.setData({
       msgList: this.data.msgList,
-      inputValue: '',
-      loading: false
     })
   },
 
@@ -54,10 +51,21 @@ Page({
       const params = {
         question
       }
+      let aiMsg = {
+        loading: true,
+        role: 'assistant',
+        content: ''
+      }
+      this.data.msgList.push(aiMsg)
+      this.setData({
+        msgList: this.data.msgList,
+      })
       const res = (await completions(params)).data || {}
       const { choices } = res
       const { message } = choices[0]
-      this.data.msgList.push(message)
+      const latestIndex = this.data.msgList.length - 1
+      this.data.msgList[latestIndex].loading = false
+      this.data.msgList[latestIndex].content = message.content
       this.setData({
         msgList: this.data.msgList,
         inputValue: '',
@@ -65,6 +73,9 @@ Page({
       })
     } catch (error) {
       console.log(error)
+      this.setData({
+        loading: false
+      })
     }
   },
 })
